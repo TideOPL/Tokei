1b5f4ad4e2db376ff1159b52f6fefbacd2fd6869
Tide <stargamerminer@gmail.com>
2024-02-12 20:50:53 +0000

    fix: potential fix for meta tags

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -4,42 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Tokei - Browse</title>
-        <meta
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-          name="viewport"
-        />
-        <meta name="theme-color" content="#d926a9" />
-
-        <meta
-          name="description"
-          content="Discover Tokei 時計, a groundbreaking open-source initiative revolutionizing live streaming. Dive into a community of innovators, developers, and creators shaping the future of real-time content delivery. Explore cutting-edge technology and unleash your creativity with Tokei 時計."
-        />
-
-        <meta property="og:url" content="https://tokei.live" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Categories - Tokei" />
-        <meta
-          property="og:description"
-          content="Discover Tokei 時計, a groundbreaking open-source initiative revolutionizing live streaming. Dive into a community of innovators, developers, and creators shaping the future of real-time content delivery. Explore cutting-edge technology and unleash your creativity with Tokei 時計."
-        />
-        <meta
-          property="og:image"
-          content="https://www.tokei.live/android-chrome-512x512.png"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="tokei.live" />
-        <meta property="twitter:url" content="https://tokei.live" />
-        <meta name="twitter:title" content="Categories - Tokei" />
-        <meta
-          name="twitter:description"
-          content="Discover Tokei 時計, a groundbreaking open-source initiative revolutionizing live streaming. Dive into a community of innovators, developers, and creators shaping the future of real-time content delivery. Explore cutting-edge technology and unleash your creativity with Tokei 時計."
-        />
-        <meta
-          name="twitter:image"
-          content="https://www.tokei.live/android-chrome-512x512.png"
-        />
         <meta
           name="theme-color"
           content="#d926a9"
