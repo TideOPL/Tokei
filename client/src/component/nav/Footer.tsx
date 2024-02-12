702fabf7e2401b1a19a5760368a25563b4931078
Andreas <Andyperry4002@gmail.com>
2024-02-12 22:30:53 +0000

    feat: Added footer

--- client/src/component/nav/Footer.tsx
+++ client/src/component/nav/Footer.tsx
@@ -1,28 +1,103 @@
 import React from "react";
-import SocialLink from "../ui/social-link";
-import { FaXTwitter } from "react-icons/fa6";
+import {
+  FaDiscord,
+  FaGithub,
+  FaInstagram,
+  FaTiktok,
+  FaXTwitter,
+} from "react-icons/fa6";
 import Socials from "./Socials";
+import Link from "next/link";
 
 const Footer = (): JSX.Element => {
   return (
     <div className="flex min-h-[400px] w-full flex-col items-center justify-items-end px-3 py-16">
-      <div className="font-noto-sans text-6xl font-bold text-white">TOKEI</div>
+      <div className="font-noto-sans text-3xl font-bold text-white">TOKEI</div>
+      <div className="pt-4 text-center font-noto-sans text-sm font-semibold text-white">
+        Our Policy
+        <div className="flex flex-row gap-x-2 pt-2 font-noto-sans text-sm text-gray-500">
+          <span>
+            <FooterLink title={"Community Guidelines"} href={"/"} />
+          </span>
+          |
+          <span>
+            <FooterLink title={"Terms of Service"} href={"/"} />
+          </span>
+          |
+          <span>
+            <FooterLink title={"Privacy Policy"} href={"/"} />
+          </span>
+          |
+          <span>
+            <FooterLink title={"DMCA Policy"} href={"/"} />
+          </span>
+          |
+          <span>
+            <FooterLink title={"Help & Support"} href={"/"} />
+          </span>
+        </div>
+      </div>
+      <div className="pt-4 text-center font-noto-sans text-sm font-semibold text-white">
+        Contact Us
+        <div className="flex flex-row gap-x-2 pt-2 font-noto-sans text-sm text-gray-500">
+          <span>
+            <FooterLink title={"Support"} href={"mailto:support@tokei.live"} />
+          </span>
+          |
+          <span>
+            <FooterLink
+              title={"Partners"}
+              href={"mailto:partners@tokei.live"}
+            />
+          </span>
+        </div>
+      </div>
       <div>
-        <div className="space-y-1.5">
+        <div className=" flex flex-row space-x-7 pt-12 text-2xl">
           <Socials
             icon={
-              <FaXTwitter
-                className="mt-1 text-3xl"
-                fill="inherit"
-                strokeWidth={0}
-              />
+              <FaXTwitter className="mt-1" fill="inherit" strokeWidth={0} />
             }
-            link={"https://X.com"}
-            title="X"
+            link={"https://X.com/@TokeiLive"}
+          />
+          <Socials
+            icon={<FaTiktok className="mt-1" fill="inherit" stroke="inherit" />}
+            link={"https://tiktok.com"}
+          />
+          <Socials
+            icon={
+              <FaDiscord className="mt-1" fill="inherit" stroke="inherit" />
+            }
+            link={"https://discord.com"}
+          />
+          <Socials
+            icon={
+              <FaInstagram className="mt-1" fill="inherit" stroke="inherit" />
+            }
+            link={"https://Instagram.com"}
+          />
+          <Socials
+            icon={<FaGithub className="mt-1" fill="inherit" stroke="inherit" />}
+            link={"https://github.com"}
           />
         </div>
       </div>
     </div>
+  );
+};
+
+interface FooterLinkProp {
+  title: string;
+  href: string;
+}
+
+const FooterLink = ({ title, href }: FooterLinkProp) => {
+  return (
+    <Link href={href} aria-label={title} className="group">
+      <div className="text-gray-500 transition-colors group-hover:text-white">
+        {title}
+      </div>
+    </Link>
   );
 };
 
--- client/src/component/nav/Socials.tsx
+++ client/src/component/nav/Socials.tsx
@@ -1,23 +1,19 @@
 import Link from "next/link";
 
 interface Props {
-  title: string;
   link: string;
   icon: JSX.Element;
 }
 
-const Socials = ({ title, link, icon }: Props) => {
+const Socials = ({ link, icon }: Props) => {
   return (
     <Link
       target="_blank"
       href={link}
       className="group flex flex-row space-x-1.5 text-white "
     >
-      <div className="fill-zinc-500 stroke-zinc-700 pt-0.5 transition-all group-hover:fill-white group-hover:stroke-zinc-500">
+      <div className="fill-zinc-400 stroke-zinc-700 pt-0.5 transition-all group-hover:fill-primary group-hover:stroke-primary">
         {icon}
-      </div>
-      <div className="text-3xl text-zinc-500 transition-all group-hover:text-primary_lighter  group-hover:underline">
-        {title}
       </div>
     </Link>
   );
