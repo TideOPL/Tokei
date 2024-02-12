ab36f549d3f95fcc325c9551ad2ff4a77ed7527b
Andreas <Andyperry4002@gmail.com>
2024-02-12 20:32:13 +0000

    testing to see if removing OG:image fixes anything

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -84,10 +84,6 @@ export default function Home() {
           property="og:description"
           content="Discover Tokei 時計, a groundbreaking open-source initiative revolutionizing live streaming. Dive into a community of innovators, developers, and creators shaping the future of real-time content delivery. Explore cutting-edge technology and unleash your creativity with Tokei 時計."
         />
-        <meta
-          property="og:image:secure"
-          content="https://www.tokei.live/android-chrome-512x512.png"
-        />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="tokei.live" />
@@ -96,10 +92,6 @@ export default function Home() {
         <meta
           name="twitter:description"
           content="Discover Tokei 時計, a groundbreaking open-source initiative revolutionizing live streaming. Dive into a community of innovators, developers, and creators shaping the future of real-time content delivery. Explore cutting-edge technology and unleash your creativity with Tokei 時計."
-        />
-        <meta
-          name="twitter:image:secure"
-          content="https://www.tokei.live/android-chrome-512x512.png"
         />
       </Head>
       {/**@ts-ignore**/}
