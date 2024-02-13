0baf7e7afa4bf1274e09ebf32ada431af8ff13d8
Tide <stargamerminer@gmail.com>
2024-02-13 16:58:03 +0000

    fix: fixes to webhook

--- server/src/api/v1/webhook.ts
+++ server/src/api/v1/webhook.ts
@@ -7,59 +7,57 @@ import makeKey from '../../util/makeKey';
 import { User } from '../../model/user';
 import { Stream } from '../../model/stream';
 import getColor from '../../util/getColor';
+import { buffer } from 'micro';
 
 export const config = {
   api: {
     bodyParser: false,
-  }
-}
+  },
+};
 
 const router: Router = express.Router();
 // POST /signup
 router.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req: Request, res: Response) => {
-  // Check if the 'Signing Secret' from the Clerk Dashboard was correctly provided
+  if (req.method !== 'POST') {
+    return res.status(405);
+  }
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+ 
   if (!WEBHOOK_SECRET) {
-    throw new Error('You need a WEBHOOK_SECRET in your .env');
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
   }
  
-  // Grab the headers and body
-  const headers = req.headers;
-  const payload = req.body;
+  // Get the headers
+  const svixId = req.headers['svix-id'] as string;
+  const svixTimestamp = req.headers['svix-timestamp'] as string;
+  const svixSignature = req.headers['svix-signature'] as string;
  
-  // Get the Svix headers for verification
-  const svixId = headers['svix-id'] as string;
-  const svixTimestamp = headers['svix-timestamp'] as string;
-  const svixSignature = headers['svix-signature'] as string;
  
-  // If there are missing Svix headers, error out
+  // If there are no headers, error out
   if (!svixId || !svixTimestamp || !svixSignature) {
-    return new Response('Error occured -- no svix headers', {
-      status: 400,
-    });
+    return res.status(400).json({ error: 'Error occured -- no svix headers' });
   }
  
-  // Initiate Svix
+  console.log('headers', req.headers, svixId, svixSignature, svixTimestamp);
+  // Get the body
+  const body = (await buffer(req)).toString();
+ 
+  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
  
   let evt: WebhookEvent;
  
-  // Attempt to verify the incoming webhook
-  // If successful, the payload will be available from 'evt'
-  // If the verification fails, error out and  return error code
+  // Verify the payload with the headers
   try {
-    evt = wh.verify(payload, {
+    evt = wh.verify(body, {
       'svix-id': svixId,
       'svix-timestamp': svixTimestamp,
       'svix-signature': svixSignature,
     }) as WebhookEvent;
-  } catch (err: any) {
-    // Console log and return errro
-    console.warn('Webhook failed to verify. Error:', err.message);
-    return res.status(400).json({
-      success: false,
-      message: err.message,
-    });
+  } catch (err) {
+    console.error('Error verifying webhook:', err);
+    return res.status(400).json({ 'Error': err });
   }
  
   // Grab the ID and TYPE of the Webhook
