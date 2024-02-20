70657d31403bc5c79898251c486b2410af0a4637
Tide <stargamerminer@gmail.com>
2024-02-20 22:11:29 +0000

    feat: added untimeout and checkTimeout

--- server/src/api/v1/routes/moderation.ts
+++ server/src/api/v1/routes/moderation.ts
@@ -5,7 +5,7 @@ import { User } from '../../../model/user';
 import { Moderator } from '../../../model/moderator';
 import { Timeout } from '../../../model/timeout';
 import { getOrSetCache } from '../../../util/cache';
-import { io } from '../../../app';
+import { io, redis } from '../../../app';
 import bodyParser from 'body-parser';
 
 const getModerator = async (moderator_id: string, channel_id: string) => {
@@ -88,6 +88,70 @@ router.post('/timeoutUser', ClerkExpressRequireAuth(), bodyParser.json(), async 
   } 
 });
 
+router.post('/unTimeoutUser', ClerkExpressRequireAuth(), bodyParser.json(), async (req: RequireAuthProp<Request>, res: Response) => {
+  try {
+    if (req.body.user == null || req.body.channel == null || req.body.timestampEnd == null || req.body.reason == null) {
+      console.log(req.body);
+      res.status(400).send();
+      return;
+    }
+
+    const user = await getOrSetCache(req.body.user, async () => {
+      const data = getUserById(req.body.user);
+      
+      return data;
+    }) as any;
+
+    const moderator = await getOrSetCache(req.auth.userId, async () => {
+      const data = getUserById(req.auth.userId);
+      
+      return data;
+    }) as any;
+
+    const channel = await getOrSetCache(req.body.channel, async () => {
+      const data = getUserById(req.body.channel);
+      
+      return data;
+    }) as any;
+
+    if (!moderator || !moderator.clerk_id || !user || !channel) {
+      res.status(404).send();
+      return;
+    }
+
+    // check if user that is being muted is the channel owner
+    if (user.clerk_id == channel.clerk_id) {
+      res.status(403).send();
+      return;
+    }
+    
+    // check if user that is being muted is a moderator
+    const isUserMod = await getModerator(user.clerk_id, req.body.channel.toString());
+    if (isUserMod) {
+      res.status(403).send();
+      return;
+    }
+
+    const modObj = await getModerator(moderator.clerk_id, req.body.channel.toString());
+
+    if (modObj || channel.clerk_id == req.auth.userId) {
+      const timeout = await Timeout.findOneAndUpdate({ active: true, channel_id: req.body.channel, user_id: req.body.user }, { active: false }).exec();
+
+      if (timeout) {
+        res.status(200).send(timeout);
+        io.sockets.emit(`chat_${channel.username}`, `@unban-${user.username}-reason-${timeout.reason}-end-${timeout.timestamp_mutedEnd}-moderator-${timeout.mod_id}`);
+        redis.del(`${user.username}__timeout`);
+      }
+
+      res.status(404).send(timeout);
+    }
+    res.status(401).send();
+  } catch (e) {
+    res.status(500).send(e?.toString());
+  } 
+});
+
+
 router.get('/amITimedOut', ClerkExpressRequireAuth(), async (req: RequireAuthProp<Request>, res: Response) => {
   try {
     if (req.query.channel == undefined || req.query.channel == null) {
@@ -130,5 +194,56 @@ router.get('/amITimedOut', ClerkExpressRequireAuth(), async (req: RequireAuthPro
     res.status(500).send(e?.toString());
   } 
 });
+
+router.get('/checkTimeout', ClerkExpressRequireAuth(), async (req: RequireAuthProp<Request>, res: Response) => {
+  try {
+    if (req.query.channel == null || req.query.user == null) {
+      res.status(400).send();
+      return;
+    }
+
+    const user = await getOrSetCache(req.query.user.toString(), async () => {
+      if (req.query.user) {
+        const data = getUserById(req.query.user?.toString());
+
+        return data;
+      }
+      
+    }) as any;
+
+    const moderator = await getOrSetCache(req.auth.userId, async () => {
+      const data = getUserById(req.auth.userId);
+      
+      return data;
+    }) as any;
+
+    const channel = await getOrSetCache(req.query.channel.toString(), async () => {
+      if (req.query.channel) {
+        const data = getUserById(req.query.channel.toString());
+      
+        return data;
+      }
+
+    }) as any;
+
+    if (!moderator || !moderator.clerk_id || !user || !channel) {
+      res.status(404).send();
+      return;
+    }
+
+    const timeout = await getTimeOutByUserId(user.clerk_id, channel.clerk_id);
+
+    if (timeout != null) {
+      res.status(302).send(timeout);
+      return;
+    }
+
+    res.status(200).send(timeout);
+    return;
+  } catch (e) {
+    res.status(500).send(e?.toString());
+  } 
+});
+
 
 export default router;
